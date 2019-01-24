ac02acfa8b2c9fd61ae56e2fb330a8bb81f6663b
Sam Warnick <sam.t.warnick@gmail.com>
2019-01-24 08:36:21 -0700

    Add tailwind plugin for prefers-color-scheme

--- tailwind.js
+++ tailwind.js
@@ -1,3 +1,4 @@
+const postcss = require("postcss");
 /*
 
 Tailwind - The Utility-First CSS Framework
@@ -837,7 +838,7 @@ module.exports = {
   modules: {
     appearance: ["responsive"],
     backgroundAttachment: ["responsive"],
-    backgroundColors: ["responsive", "hover", "focus"],
+    backgroundColors: ["responsive", "hover", "focus", "dark"],
     backgroundPosition: ["responsive"],
     backgroundRepeat: ["responsive"],
     backgroundSize: ["responsive"],
@@ -902,14 +903,21 @@ module.exports = {
   */
 
   plugins: [
-    require("tailwindcss/plugins/container")({
-      // center: true,
-      // padding: '1rem',
-    }),
-    function({ addVariant }) {
+    ({ addVariant }) => {
       addVariant("disabled", ({ modifySelectors, separator }) => {
         modifySelectors(({ className }) => {
           return `.disabled${separator}${className}:disabled`;
+        });
+      });
+      addVariant("dark", ({ container, separator }) => {
+        const mediaRule = postcss.atRule({
+          name: "media",
+          params: "(prefers-color-scheme: dark)"
+        });
+        mediaRule.nodes = container.nodes;
+        container.nodes = [mediaRule];
+        mediaRule.walkRules(rule => {
+          rule.selector = `.dark${separator}${rule.selector.slice(1)}`;
         });
       });
     }
