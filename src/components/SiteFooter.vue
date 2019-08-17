4b65f7eab134e889027834e257b3aed75a48e8b6
Sam Warnick <sam.t.warnick@gmail.com>
2019-08-17 12:32:24 -0600

    Add links to RSS and sitemap

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,29 +1,17 @@
 <template>
-  <footer class="p-12 mt-12 text-sm text-gray-600 flex flex-col items-center">
-<!--    <nav>-->
-<!--      <ul class="list-reset flex flex-col sm:flex-row">-->
-<!--        <li class="flex mb-2">-->
-<!--          <RouterLink to="/resume/" class="text-grey-darker hover:text-grey-darkest">Résumé</RouterLink>-->
-<!--          <span class="mx-2 hidden sm:inline-block">•</span>-->
-<!--        </li>-->
-<!--        <li class="flex mb-2">-->
-<!--          <RouterLink to="/contact/" class="text-grey-darker hover:text-grey-darkest">Contact</RouterLink>-->
-<!--          <span class="mx-2 hidden sm:inline-block">•</span>-->
-<!--        </li>-->
-<!--        <li class="flex mb-2">-->
-<!--          <RouterLink to="/gear/" class="text-grey-darker hover:text-grey-darkest">Gear</RouterLink>-->
-<!--          <span class="mx-2 hidden sm:inline-block">•</span>-->
-<!--        </li>-->
-<!--        <li class="flex mb-2">-->
-<!--          <RouterLink to="/category/" class="text-grey-darker hover:text-grey-darkest">Categories</RouterLink>-->
-<!--          <span class="mx-2 hidden sm:inline-block">•</span>-->
-<!--        </li>-->
-<!--        <li class="flex mb-2">-->
-<!--          <RouterLink to="/tag/" class="text-grey-darker hover:text-grey-darkest">Tags</RouterLink>-->
-<!--        </li>-->
-<!--      </ul>-->
-<!--    </nav>-->
-    <p class="mt-2 text-grey-darker text-center">copyright 2016 – {{currentYear}} sam warnick</p>
+  <footer class="p-12 mt-12 text-sm text-gray-600 flex flex-col sm:items-center">
+    <nav class="mb-4">
+      <ul class="list-reset flex flex-col sm:flex-row">
+        <li class="flex mb-2">
+          <a href="/feed.xml" class="text-gray-600 hover:text-gray-800 transition">RSS</a>
+          <span class="mx-2 hidden sm:inline-block">•</span>
+        </li>
+        <li class="flex mb-2">
+          <a href="/sitemap.xml" class="text-gray-600 hover:text-gray-800 transition">Sitemap</a>
+        </li>
+      </ul>
+    </nav>
+    <p class="mt-2 text-grey-darker sm:text-center">copyright 2016 – {{currentYear}} sam warnick</p>
   </footer>
 </template>
 
