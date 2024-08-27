ec86f75b5e61e6561838e8e607e367827d20eb60
Sam Warnick <sam@warnick.me>
2024-08-27 19:40:48 -0400

    Add search closes #62

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ import pluginRss from "@11ty/eleventy-plugin-rss";
 import eleventyPluginOgImage from "eleventy-plugin-og-image";
 import lightningCSS from "@11tyrocks/eleventy-plugin-lightningcss";
 import fs from "fs";
+import { execSync } from "child_process";
 import { DateTime } from "luxon";
 
 import MarkdownIt from "markdown-it";
@@ -13,156 +14,160 @@ import Shiki from "@shikijs/markdown-it";
 
 /** @param {import('@11ty/eleventy/src/UserConfig').default} eleventyConfig */
 export default async function (eleventyConfig) {
-	eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+  eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 
-	eleventyConfig.addDateParsing(function (dateValue) {
-		if (dateValue) {
-			return DateTime.fromISO(dateValue).setZone("America/New_York");
-		}
-	});
+  eleventyConfig.addDateParsing(function (dateValue) {
+    if (dateValue) {
+      return DateTime.fromISO(dateValue).setZone("America/New_York");
+    }
+  });
 
-	const options = {
-		html: true,
-		breaks: true,
-		linkify: true,
-	};
+  const options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
 
-	let markdownLib = MarkdownIt(options)
-		.use(
-			await Shiki({
-				theme: "rose-pine-moon",
-			}),
-		)
-		.use(MarkdownItGitHubAlerts)
-		.use(MarkdownItFootnote)
-		.use(html5Media)
-		.use(MarkdownItAttrs);
+  let markdownLib = MarkdownIt(options)
+    .use(
+      await Shiki({
+        theme: "rose-pine-moon",
+      }),
+    )
+    .use(MarkdownItGitHubAlerts)
+    .use(MarkdownItFootnote)
+    .use(html5Media)
+    .use(MarkdownItAttrs);
 
-	markdownLib.renderer.rules.footnote_anchor = (
-		tokens,
-		idx,
-		options,
-		env,
-		slf,
-	) => {
-		var id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
+  markdownLib.renderer.rules.footnote_anchor = (
+    tokens,
+    idx,
+    options,
+    env,
+    slf,
+  ) => {
+    var id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
 
-		if (tokens[idx].meta.subId > 0) {
-			id += ":" + tokens[idx].meta.subId;
-		}
+    if (tokens[idx].meta.subId > 0) {
+      id += ":" + tokens[idx].meta.subId;
+    }
 
-		return ' <a href="#fnref' + id + '">&#10558;</a>';
-	};
+    return ' <a href="#fnref' + id + '">&#10558;</a>';
+  };
 
-	eleventyConfig.setLibrary("md", markdownLib);
+  eleventyConfig.setLibrary("md", markdownLib);
 
-	eleventyConfig.addPlugin(lightningCSS);
-	eleventyConfig.addPlugin(pluginRss);
-	eleventyConfig.addPlugin(eleventyPluginOgImage, {
-		satoriOptions: {
-			fonts: [
-				{
-					name: "Atkinson",
-					data: fs.readFileSync(
-						"./src/assets/fonts/Atkinson-Hyperlegible-Bold-102.woff",
-					),
-					weight: 700,
-					style: "normal",
-				},
-				{
-					name: "Calistoga",
-					data: fs.readFileSync("./src/assets/fonts/Calistoga-Regular.ttf"),
-					weight: 400,
-					style: "normal",
-				},
-			],
-		},
-	});
+  eleventyConfig.addPlugin(lightningCSS);
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(eleventyPluginOgImage, {
+    satoriOptions: {
+      fonts: [
+        {
+          name: "Atkinson",
+          data: fs.readFileSync(
+            "./src/assets/fonts/Atkinson-Hyperlegible-Bold-102.woff",
+          ),
+          weight: 700,
+          style: "normal",
+        },
+        {
+          name: "Calistoga",
+          data: fs.readFileSync("./src/assets/fonts/Calistoga-Regular.ttf"),
+          weight: 400,
+          style: "normal",
+        },
+      ],
+    },
+  });
 
-	eleventyConfig.addShortcode("timestamp", function () {
-		return Date.now();
-	});
-	eleventyConfig.addShortcode("year", function () {
-		return new Date().getFullYear();
-	});
-	eleventyConfig.addFilter("formattedDate", function (date) {
-		return DateTime.fromJSDate(date, { zone: "utc" }).toLocaleString(
-			DateTime.DATE_MED,
-		);
-	});
-	eleventyConfig.addFilter("getAllTags", function (collection) {
-		let tagSet = new Set();
-		for (let item of collection) {
-			(item.data.tags || []).forEach((tag) => tagSet.add(tag));
-		}
-		return Array.from(tagSet);
-	});
-	eleventyConfig.addFilter("removeSite", function (url) {
-		return url.replace("https://samwarnick.com", "");
-	});
-	eleventyConfig.addFilter("toGitHub", function (inputPath) {
-		return `https://github.com/samwarnick/samwarnick.com/blob/main${inputPath.substring(1)}`;
-	});
-	eleventyConfig.addFilter("dateToRfc822WithCorrectTz", function (date) {
-		return DateTime.fromJSDate(date).toRFC2822();
-	});
-	eleventyConfig.addFilter("defaultImage", function (image) {
-		const defaultImages = [
-			"og-image/1.jpg",
-			"og-image/2.jpg",
-			"og-image/3.jpg",
-			"og-image/4.jpg",
-			"og-image/5.jpg",
-			"og-image/6.jpg",
-			"og-image/7.jpg",
-			"og-image/8.jpg",
-			"og-image/9.jpg",
-			"og-image/10.jpg",
-			"og-image/11.jpg",
-			"og-image/12.jpg",
-		];
-		return (
-			image || defaultImages[Math.floor(Math.random() * defaultImages.length)]
-		);
-	});
-	eleventyConfig.addFilter("something", function (value) {
-		console.log(value);
-		return value.replace("&#39;", ",");
-	});
+  eleventyConfig.addShortcode("timestamp", function () {
+    return Date.now();
+  });
+  eleventyConfig.addShortcode("year", function () {
+    return new Date().getFullYear();
+  });
+  eleventyConfig.addFilter("formattedDate", function (date) {
+    return DateTime.fromJSDate(date, { zone: "utc" }).toLocaleString(
+      DateTime.DATE_MED,
+    );
+  });
+  eleventyConfig.addFilter("getAllTags", function (collection) {
+    let tagSet = new Set();
+    for (let item of collection) {
+      (item.data.tags || []).forEach((tag) => tagSet.add(tag));
+    }
+    return Array.from(tagSet);
+  });
+  eleventyConfig.addFilter("removeSite", function (url) {
+    return url.replace("https://samwarnick.com", "");
+  });
+  eleventyConfig.addFilter("toGitHub", function (inputPath) {
+    return `https://github.com/samwarnick/samwarnick.com/blob/main${inputPath.substring(1)}`;
+  });
+  eleventyConfig.addFilter("dateToRfc822WithCorrectTz", function (date) {
+    return DateTime.fromJSDate(date).toRFC2822();
+  });
+  eleventyConfig.addFilter("defaultImage", function (image) {
+    const defaultImages = [
+      "og-image/1.jpg",
+      "og-image/2.jpg",
+      "og-image/3.jpg",
+      "og-image/4.jpg",
+      "og-image/5.jpg",
+      "og-image/6.jpg",
+      "og-image/7.jpg",
+      "og-image/8.jpg",
+      "og-image/9.jpg",
+      "og-image/10.jpg",
+      "og-image/11.jpg",
+      "og-image/12.jpg",
+    ];
+    return (
+      image || defaultImages[Math.floor(Math.random() * defaultImages.length)]
+    );
+  });
+  eleventyConfig.addFilter("something", function (value) {
+    console.log(value);
+    return value.replace("&#39;", ",");
+  });
 
-	eleventyConfig.addPassthroughCopy("src/assets/fonts");
-	eleventyConfig.addPassthroughCopy("src/assets/favicon");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts");
+  eleventyConfig.addPassthroughCopy("src/assets/favicon");
 
-	eleventyConfig.addCollection("posts", function (collectionApi) {
-		return collectionApi.getFilteredByGlob("src/content/blog/**/*.md");
-	});
-	eleventyConfig.addCollection("currentProjects", function (collectionApi) {
-		return collectionApi.getFilteredByGlob("src/content/projects/**/*.md");
-	});
-	eleventyConfig.addCollection("postsByYear", function (collectionApi) {
-		const posts = collectionApi.getFilteredByGlob("src/content/blog/**/*.md");
-		const years = posts.map((post) => post.date.getFullYear());
-		const uniqueYears = [...new Set(years)];
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/content/blog/**/*.md");
+  });
+  eleventyConfig.addCollection("currentProjects", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/content/projects/**/*.md");
+  });
+  eleventyConfig.addCollection("postsByYear", function (collectionApi) {
+    const posts = collectionApi.getFilteredByGlob("src/content/blog/**/*.md");
+    const years = posts.map((post) => post.date.getFullYear());
+    const uniqueYears = [...new Set(years)];
 
-		const postByYear = uniqueYears.reduce((acc, year) => {
-			const filteredPosts = posts.filter(
-				(post) => post.date.getFullYear() === year,
-			);
+    const postByYear = uniqueYears.reduce((acc, year) => {
+      const filteredPosts = posts.filter(
+        (post) => post.date.getFullYear() === year,
+      );
 
-			return [...acc, { year, posts: filteredPosts }];
-		}, []);
+      return [...acc, { year, posts: filteredPosts }];
+    }, []);
 
-		return postByYear;
-	});
-	eleventyConfig.addCollection("Devlog", function (collectionApi) {
-		const posts = collectionApi.getFilteredByGlob("src/content/blog/**/*.md");
+    return postByYear;
+  });
+  eleventyConfig.addCollection("Devlog", function (collectionApi) {
+    const posts = collectionApi.getFilteredByGlob("src/content/blog/**/*.md");
 
-		return posts.filter((post) => post.data.tags.includes("Devlog"));
-	});
+    return posts.filter((post) => post.data.tags.includes("Devlog"));
+  });
 
-	return {
-		dir: {
-			input: "src",
-		},
-	};
+  eleventyConfig.on("eleventy.after", () => {
+    execSync(`npx pagefind --site _site`, { encoding: "utf-8" });
+  });
+
+  return {
+    dir: {
+      input: "src",
+    },
+  };
 }
