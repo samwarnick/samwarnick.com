42e4e0d176f657cbee19d3c4b84764ade85626f3
Sam Warnick <sam@warnick.me>
2024-10-26 11:12:01 -0400

    More optimizations

--- src/assets/js/tcg-card.js
+++ src/assets/js/tcg-card.js
@@ -5,122 +5,122 @@ class TCGCard extends HTMLElement {
 		super();
 		const template = document.createElement("template");
 		template.innerHTML = `
-    <style>
-    :host {
-      display: block;
+<style>
+:host {
+	display: block;
 
-      --display-scale: 0.5;
-      --display-tx: 0;
-      --display-ty: 0;
-      --display-rx: 0deg;
-      --display-ry: 0deg;
-      --display-rz: 0deg;
+	--display-scale: 0.5;
+	--display-tx: 0;
+	--display-ty: 0;
+	--display-rx: 0deg;
+	--display-ry: 0deg;
+	--display-rz: 0deg;
 
-      --pointer-x: 50%;
-      --pointer-y: 50%;
+	--pointer-x: 50%;
+	--pointer-y: 50%;
 
-      --glare-opacity: 0;
-      --z-index: 1;
+	--glare-opacity: 0;
+	--z-index: 1;
 
-      z-index: var(--z-index);
-      pointer-events: none;
-    }
+	z-index: var(--z-index);
+	pointer-events: none;
+}
 
-    .tcg-wrapper {
-      aspect-ratio: 733/1024;
-      width: 100%;
-      position: relative;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      perspective: 30cm;
-      pointer-events: none;
-      border-radius: 4%;
+.tcg-wrapper {
+	aspect-ratio: 733/1024;
+	width: 100%;
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	perspective: 30cm;
+	pointer-events: none;
+	border-radius: 4%;
 
-      &:has(:focus-visible) {
-      	outline: 3px solid var(--focus-outline, black);
-       	outline-offset: 3px;
-      }
-    }
+	&:has(:focus-visible) {
+		outline: 3px solid var(--focus-outline, black);
+		outline-offset: 3px;
+	}
+}
 
-    .tcg-proxy {
-      width: 100%;
-      aspect-ratio: 733/1024;
-      pointer-events: none;
-      
-      &:after {
-		  content: "";
-		  position: absolute;
-		  inset: 10px;
-		  border-radius: 4%;
-		  background-color: rgba(0,0,0,0.4);
-		  border: 2px solid rgba(0,0,0,0.5) ;
-		  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1' id='Pokéball' x='0px' y='0px' viewBox='0 0 595.3 594.1' style='enable-background:new 0 0 595.3 594.1;' xml:space='preserve'%3E%3Cstyle type='text/css'%3E .st0%7Bfill:%23FFFFFF;%7D .st1%7Bfill:%23DFDFDF;%7D .st2%7Bfill:%23FF1C1C;%7D .st3%7Bfill:%23DF1818;%7D%0A%3C/style%3E%3Cg id='Pokéball_1_'%3E%3Cg id='Colours'%3E%3Cpath id='Down' class='st0' d='M297.6,380.9c-40.4,0-74.1-28.6-82.1-66.6H81.1c9.5,110.5,102.2,197.2,215.1,197.2 s205.7-86.7,215.1-197.2H379.7C371.7,352.4,338,380.9,297.6,380.9z'/%3E%3Cpath id='Shadow_Down' class='st1' d='M345.6,505.9c89.6-21,157.7-97.7,165.7-191.6h-53C453,399.5,408.3,471.7,345.6,505.9z'/%3E%3Cpath id='Center' class='st0' d='M347.1,297L347.1,297C347,297,347,297,347.1,297c-0.1-6.1-1.2-11.9-3.2-17.3 c-7-18.8-25.1-32.1-46.3-32.1s-39.3,13.4-46.3,32.1c-2,5.4-3.1,11.2-3.1,17.3c0,0,0,0,0,0h0.1c0,0,0,0,0,0 c0,6.1,1.1,11.9,3.1,17.3c7,18.8,25.1,32.1,46.3,32.1c21.2,0,39.3-13.4,46.3-32.1C346,309,347.1,303.1,347.1,297 C347.1,297,347.1,297,347.1,297z'/%3E%3Cpath id='Up' class='st2' d='M297.7,213.2c40.4,0,74.1,28.6,82.1,66.6h134.4C504.7,169.2,412,82.5,299,82.5S93.4,169.2,83.9,279.7 h131.7C223.6,241.7,257.3,213.2,297.7,213.2z'/%3E%3Cpath id='Shadow_Up' class='st3' d='M458.3,279.7h55.8c-8.2-95.5-78.6-173.3-170.5-192.6C407.4,120.8,452.9,193.7,458.3,279.7z'/%3E%3C/g%3E%3Cpath id='Line' d='M299,82.5c113,0,205.7,86.7,215.1,197.2H379.7c-8-38-41.7-66.6-82.1-66.6c-40.4,0-74.1,28.6-82.1,66.6H83.9 C93.4,169.2,186.1,82.5,299,82.5z M343.9,279.7c2,5.4,3.1,11.2,3.1,17.3c0,0,0,0,0,0h0.1c0,0,0,0,0,0c0,6.1-1.1,11.9-3.1,17.3 c-7,18.8-25.1,32.1-46.3,32.1c-21.2,0-39.3-13.4-46.3-32.1c-2-5.4-3.1-11.2-3.1-17.3c0,0,0,0,0,0h-0.1c0,0,0,0,0,0 c0-6.1,1.1-11.9,3.1-17.3c7-18.8,25.1-32.1,46.3-32.1S336.9,261,343.9,279.7z M296.2,511.6c-113,0-205.7-86.7-215.1-197.2h134.4 c8,38,41.7,66.6,82.1,66.6s74.1-28.6,82.1-66.6h131.7C501.9,424.8,409.2,511.6,296.2,511.6z M297.6,41.3 C156.4,41.3,41.9,155.8,41.9,297s114.5,255.7,255.7,255.7S553.4,438.3,553.4,297S438.9,41.3,297.6,41.3z'/%3E%3C/g%3E%3Cscript xmlns=''/%3E%3C/svg%3E");
-		  background-repeat: no-repeat;
-		  background-position: center;
-		  background-blend-mode: screen;
-		  filter: saturate(0) opacity(0.1);
-      }
-    }
+.tcg-proxy {
+	width: 100%;
+	aspect-ratio: 733/1024;
+	pointer-events: none;
+	
+	&:after {
+		content: "";
+		position: absolute;
+		inset: 10px;
+		border-radius: 4%;
+		background-color: rgba(0,0,0,0.4);
+		border: 2px solid rgba(0,0,0,0.5) ;
+		background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg enable-background='new 0 0 595.3 594.1' version='1.1' viewBox='0 0 595.3 594.1' xml:space='preserve' xmlns='http://www.w3.org/2000/svg'%3E%3Cstyle type='text/css'%3E .st0%7Bfill:%23FFFFFF;%7D .st1%7Bfill:%23DFDFDF;%7D .st2%7Bfill:%23FF1C1C;%7D .st3%7Bfill:%23DF1818;%7D%0A%3C/style%3E%3Cpath class='st0' d='m297.6 380.9c-40.4 0-74.1-28.6-82.1-66.6h-134.4c9.5 110.5 102.2 197.2 215.1 197.2s205.7-86.7 215.1-197.2h-131.6c-8 38.1-41.7 66.6-82.1 66.6z'/%3E%3Cpath class='st1' d='m345.6 505.9c89.6-21 157.7-97.7 165.7-191.6h-53c-5.3 85.2-50 157.4-112.7 191.6z'/%3E%3Cpath class='st0' d='m347.1 297c-0.1 0-0.1 0 0 0-0.1-6.1-1.2-11.9-3.2-17.3-7-18.8-25.1-32.1-46.3-32.1s-39.3 13.4-46.3 32.1c-2 5.4-3.1 11.2-3.1 17.3h0.1c0 6.1 1.1 11.9 3.1 17.3 7 18.8 25.1 32.1 46.3 32.1s39.3-13.4 46.3-32.1c2-5.3 3.1-11.2 3.1-17.3z'/%3E%3Cpath class='st2' d='m297.7 213.2c40.4 0 74.1 28.6 82.1 66.6h134.4c-9.5-110.6-102.2-197.3-215.2-197.3s-205.6 86.7-215.1 197.2h131.7c8-38 41.7-66.5 82.1-66.5z'/%3E%3Cpath class='st3' d='m458.3 279.7h55.8c-8.2-95.5-78.6-173.3-170.5-192.6 63.8 33.7 109.3 106.6 114.7 192.6z'/%3E%3Cpath d='m299 82.5c113 0 205.7 86.7 215.1 197.2h-134.4c-8-38-41.7-66.6-82.1-66.6s-74.1 28.6-82.1 66.6h-131.6c9.5-110.5 102.2-197.2 215.1-197.2zm44.9 197.2c2 5.4 3.1 11.2 3.1 17.3h0.1c0 6.1-1.1 11.9-3.1 17.3-7 18.8-25.1 32.1-46.3 32.1s-39.3-13.4-46.3-32.1c-2-5.4-3.1-11.2-3.1-17.3h-0.1c0-6.1 1.1-11.9 3.1-17.3 7-18.8 25.1-32.1 46.3-32.1s39.3 13.4 46.3 32.1zm-47.7 231.9c-113 0-205.7-86.7-215.1-197.2h134.4c8 38 41.7 66.6 82.1 66.6s74.1-28.6 82.1-66.6h131.7c-9.5 110.4-102.2 197.2-215.2 197.2zm1.4-470.3c-141.2 0-255.7 114.5-255.7 255.7s114.5 255.7 255.7 255.7 255.8-114.4 255.8-255.7-114.5-255.7-255.8-255.7z'/%3E%3C/svg%3E");
+		background-repeat: no-repeat;
+		background-position: center;
+		background-blend-mode: screen;
+		filter: saturate(0) opacity(0.1);
+	}
+}
 
-    .tcg-display {
-      pointer-events: auto;
-      border: none;
-      position: absolute;
-      scale: var(--display-scale);
-      translate: var(--display-tx) var(--display-ty);
-      transform: rotateX(var(--display-rx)) rotateY(var(--display-ry));
-      rotate: z var(--display-rz);
-      transform-origin: center;
-      background: none;
-      margin: 0;
-      padding: 0;
-      border-radius: 4%;
-      filter: drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06));
-      -webkit-tap-highlight-color: rgba(0,0,0,0);
+.tcg-display {
+	pointer-events: auto;
+	border: none;
+	position: absolute;
+	scale: var(--display-scale);
+	translate: var(--display-tx) var(--display-ty);
+	transform: rotateX(var(--display-rx)) rotateY(var(--display-ry));
+	rotate: z var(--display-rz);
+	transform-origin: center;
+	background: none;
+	margin: 0;
+	padding: 0;
+	border-radius: 4%;
+	filter: drop-shadow(0 4px 3px rgb(0 0 0 / 0.07)) drop-shadow(0 2px 2px rgb(0 0 0 / 0.06));
+	-webkit-tap-highlight-color: rgba(0,0,0,0);
 
-      transition: scale ${EXPAND_TRANSITION_TIME}ms ease-in, translate ${EXPAND_TRANSITION_TIME}ms ease-out, rotate 250ms ease-in;
+	transition: scale ${EXPAND_TRANSITION_TIME}ms ease-in, translate ${EXPAND_TRANSITION_TIME}ms ease-out, rotate 250ms ease-in;
 
-      &:focus-visible {
-      	outline: none;
-      }
-    }
+	&:focus-visible {
+		outline: none;
+	}
+}
 
-    .tcg-card {
-      display: block;
-    }
+.tcg-card {
+	display: block;
+}
 
-    .tcg-shine {
-      position: absolute;
-      inset: 0;
-    }
+.tcg-shine {
+	position: absolute;
+	inset: 0;
+}
 
-    .tcg-glare {
-      position: absolute;
-      inset: 0;
-      background-image: radial-gradient(
-        farthest-corner circle at var(--pointer-x) var(--pointer-y),
-        hsla(0, 0%, 100%, 0.8) 10%,
-        hsla(0, 0%, 100%, 0.65) 20%,
-        hsla(0, 0%, 0%, 0.5) 90%
-      );
-      mix-blend-mode: overlay;
-      opacity: var(--glare-opacity);
-      transition: opacity 500ms ease-out;
-      border-radius: 4%;
-    }
-    </style>
-    <div class="tcg-wrapper">
-      <div class="tcg-proxy">
-      </div>
-      <button class="tcg-display">
-        <img class="tcg-card" src="${this.getAttribute(
-					"src",
-				)}" alt="${this.getAttribute("alt")}" loading="lazy">
-        <div class="tcg-shine"></div>
-        <div class="tcg-glare"></div>
-      </button>
-    </div>
+.tcg-glare {
+	position: absolute;
+	inset: 0;
+	background-image: radial-gradient(
+		farthest-corner circle at var(--pointer-x) var(--pointer-y),
+		hsla(0, 0%, 100%, 0.8) 10%,
+		hsla(0, 0%, 100%, 0.65) 20%,
+		hsla(0, 0%, 0%, 0.5) 90%
+	);
+	mix-blend-mode: overlay;
+	opacity: var(--glare-opacity);
+	transition: opacity 500ms ease-out;
+	border-radius: 4%;
+}
+</style>
+<div class="tcg-wrapper">
+	<div class="tcg-proxy">
+	</div>
+	<button class="tcg-display">
+		<img class="tcg-card" src="${this.getAttribute(
+			"src",
+		)}" alt="${this.getAttribute("alt")}" loading="lazy">
+		<div class="tcg-shine"></div>
+		<div class="tcg-glare"></div>
+	</button>
+</div>
     `;
 
 		this._shadowRoot = this.attachShadow({ mode: "closed" });
