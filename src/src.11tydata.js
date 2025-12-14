d5e1560696127cce0f5fe084df758769fc87d4a1
Sam Warnick <sam@warnick.me>
2025-12-13 19:01:29 -0500

    Add meta tag

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,3 +1,14 @@
+import crypto from "node:crypto";
+
+function hash(title, backgroundImage) {
+  const hash = crypto.createHash('sha256');
+  hash.update(`${title}-${backgroundImage}`);
+  return hash.digest('hex').substring(0, 8);
+}
+
 export default {
   layout: "layouts/base.njk",
+  eleventyComputed: {
+    hash: (data) => hash(data.title, data.ogImage),
+  }
 };
